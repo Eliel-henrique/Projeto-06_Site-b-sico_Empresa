a2d81e21b2742e809021a67b546a04c37a773896
Eliel Henrique <elielhenriquedepaula07@gmail.com>
2024-02-07 17:02:46 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--fonte-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+
+    <!--Estilo-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"><!--Foi necessária para os ícones-->
+    <link rel="stylesheet" href="css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Projeto 06</title>
+</head>
+<body>
+    
+    
+
+    <section class="main">
+        <div class="layer"></div>
+        <div class="center">
+            <header>
+                <div class="icons-social">
+                    <a href="https://facebook.com"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="https://instagran.com"><i class="fa-brands fa-instagram"></i></a>
+                </div>
+                <div class="clear"></div>
+                <div class="logo">
+                    Danki Code
+                </div>
+                <nav class="desktop">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Contato</a></li>
+                    </ul>
+                </nav>
+                <nav class="mobile">
+                    <i class="fa fa-bars"></i>
+                <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Contato</a></li>
+                    </ul>
+                </nav>
+                <div class="clear"></div>
+            </header>
+            <div class="conteudo-header">
+                <h2>Bem Vindo(a) ao nosso <span>Website</span></h2>
+
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad saepe labore aut deleniti necessitatibus explicabo,
+                     tempore excepturi velit aperiam laboriosam quos ducimus mollitia incidunt unde earum quis consequuntur fuga porro.
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate corrupti, autem incidunt in, unde illum.</p>
+
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad saepe labore aut deleniti necessitatibus explicabo,
+                     tempore excepturi velit aperiam laboriosam quos ducimus mollitia incidunt unde earum quis consequuntur fuga porro.
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate corrupti, autem incidunt in, unde illum,</p>     
+                       
+                <a href="#">Saiba mais!</a>
+            </div>
+        </div>
+    </section>
+
+    <section class="planos">
+        <div class="chamada-planos">
+            <div class="center">
+                <h2>Confira nossos planos</h2>
+            </div>
+        </div>
+        <section class="box-planos">
+            <div class="center">
+                <div class="box-planos-single">
+                    <div class="box-planos-wraper">
+                        <h2>Lorem ipsun dolor</h2>
+                        <div class="pontos-plano">
+                            <p><i class="fa-solid fa-square-check"></i>Lorem ipsum silor domor</p>
+                            <p><i class="fa-solid fa-square-check"></i>Lorem ipsum silor domor</p>
+                            <p><i class="fa-solid fa-square-check"></i>Lorem ipsum silor domor</p>
+                            <p><i class="fa-solid fa-square-check"></i>Lorem ipsum silor domor</p>
+                        </div>
+                        <div class="preco">
+                            <h2>R$ 24,90</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="box-planos-single">
+                    <div class="box-planos-wraper">
+                        <h2>Lorem ipsun dolor</h2>
+                        <div class="pontos-plano">
+                            <p><i class="fa-solid fa-square-check"></i>Lorem ipsum silor domor</p>
+                            <p><i class="fa-solid fa-square-check"></i>Lorem ipsum silor domor</p>
+                            <p><i class="fa-solid fa-square-check"></i>Lorem ipsum silor domor</p>
+                            <p><i class="fa-solid fa-square-check"></i>Lorem ipsum silor domor</p>
+                        </div>
+                        <div class="preco">
+                            <h2>R$ 147,90</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="box-planos-single">
+                    <div class="box-planos-wraper">
+                        <h2>Lorem ipsun dolor</h2>
+                        <div class="pontos-plano">
+                            <p><i class="fa-solid fa-square-check"></i>Lorem ipsum silor domor</p>
+                            <p><i class="fa-solid fa-square-check"></i>Lorem ipsum silor domor</p>
+                            <p><i class="fa-solid fa-square-check"></i>Lorem ipsum silor domor</p>
+                            <p><i class="fa-solid fa-square-check"></i>Lorem ipsum silor domor</p>
+                        </div>
+                        <div class="preco">
+                            <h2>R$ 249,90</h2>
+                        </div>
+                    </div>    
+                </div>
+                </div>
+            </div>
+        </section>
+    </section>
+
+    <section class="parallax">
+        <div class="overlay-parallax"></div>
+        <div class="center">
+            <h2>Sobre nossa <span style=" color:#2692FF;">empresa</span></h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad saepe labore aut deleniti necessitatibus explicabo,
+                     tempore excepturi velit aperiam laboriosam quos ducimus mollitia incidunt unde earum quis consequuntur fuga porro.
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate corrupti, autem incidunt in, unde illum.</p>
+
+                      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad saepe labore aut deleniti necessitatibus explicabo,
+                     tempore excepturi velit aperiam laboriosam quos ducimus mollitia incidunt unde earum quis consequuntur fuga porro.
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate corrupti, autem incidunt in, unde illum.</p>
+        </div>
+    </section>
+
+    <section class="final-section">
+        <div class="center">
+            <div class="w50 contato">
+                <h2><div class="border-efeito"></div>Entre em contato!</h2>
+                <form>
+                    <input type="text" placeholder="Insira seu nome...">
+                    <input type="email" placeholder="Insira seu e-mail...">
+                    <textarea placeholder="Insira sua mensagem..."></textarea>
+                    <input type= "submit" value="ENVIAR!">
+                </form>
+            </div>
+
+            <div class="w50 time">
+                <h2><div class="border-efeito"></div>Conheça nosso time!</h2>
+                <div class="w50 equipe-single">
+                    <div class="topo-box-usuario">
+
+                        <div class="box-avatar"></div>
+                        <div class="info-usuario">
+                            <p style="font-weight:bold;">Guilherme Grilo</p>
+                            <p style="font-weight:italic;">CEO / Instrutor</p>
+                        </div>
+
+                    </div>
+
+                        <div class="descricao-usuario-box">
+                            <p>Lorem ipsum dolor sit amet consectetur 
+                                adipisicing elit. Illo itaque inventore 
+                                nulla id iusto delectus debitis vero.
+                                 Incidunt labore optio dignissimos fugiat
+                                  dolor earum alias ut? Ipsa ea incidunt 
+                                  sit.</p>
+                        </div>
+                        
+                    
+                </div>
+
+                <div class="w50 equipe-single">
+                    <div class="topo-box-usuario">
+
+                        <div class="box-avatar"></div>
+                        <div class="info-usuario">
+                            <p style="font-weight:bold;">Guilherme Grilo</p>
+                            <p style="font-weight:italic;">CEO / Instrutor</p>
+                        </div>
+
+                    </div>
+
+                        <div class="descricao-usuario-box">
+                            <p>Lorem ipsum dolor sit amet consectetur 
+                                adipisicing elit. Illo itaque inventore 
+                                nulla id iusto delectus debitis vero.
+                                 Incidunt labore optio dignissimos fugiat
+                                  dolor earum alias ut? Ipsa ea incidunt 
+                                  sit.</p>
+                        </div>
+                        
+                    
+                </div>
+
+                <div class="w50 equipe-single">
+                    <div class="topo-box-usuario">
+
+                        <div class="box-avatar"></div>
+                        <div class="info-usuario">
+                            <p style="font-weight:bold;">Guilherme Grilo</p>
+                            <p style="font-weight:italic;">CEO / Instrutor</p>
+                        </div>
+
+                    </div>
+
+                        <div class="descricao-usuario-box">
+                            <p>Lorem ipsum dolor sit amet consectetur 
+                                adipisicing elit. Illo itaque inventore 
+                                nulla id iusto delectus debitis vero.
+                                 Incidunt labore optio dignissimos fugiat
+                                  dolor earum alias ut? Ipsa ea incidunt 
+                                  sit.</p>
+                        </div>
+                        
+                    
+                </div>
+
+                <div class="w50 equipe-single">
+                    <div class="topo-box-usuario">
+
+                        <div class="box-avatar"></div>
+                        <div class="info-usuario">
+                            <p style="font-weight:bold;">Guilherme Grilo</p>
+                            <p style="font-weight:italic;">CEO / Instrutor</p>
+                        </div>
+
+                    </div>
+
+                        <div class="descricao-usuario-box">
+                            <p>Lorem ipsum dolor sit amet consectetur 
+                                adipisicing elit. Illo itaque inventore 
+                                nulla id iusto delectus debitis vero.
+                                 Incidunt labore optio dignissimos fugiat
+                                  dolor earum alias ut? Ipsa ea incidunt 
+                                  sit.</p>
+                        </div>
+                        
+                    
+                </div>
+
+                <div class="clear"></div>
+            </div>
+            <div class="clear"></div>
+        </div>
+    </section>
+
+    <footer>
+        <p>Todos os seus direitos reservados ao Projeto #6!</p>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+
+    <script>
+        $(function(){
+            $('nav.mobile i').click(function(){
+                var el = $(this).parent().find('ul');
+                if(el.is(':hidden')){
+                    el.fadeIn();   
+                }else{
+                    el.fadeOut();
+                }         
+            })
+        })
+    </script>
+
+
+</body>
+</html>
